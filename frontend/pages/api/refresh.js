acce156433356c3c8fa59f759c9c8d3d326a55dc
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-09-13 08:44:04 -0300

    Aula 04.04 - Middleware de Refresh

--- frontend/pages/api/refresh.js
+++ frontend/pages/api/refresh.js
@@ -1,4 +1,6 @@
 import nookies from 'nookies'
+import { HttpClient } from '../../src/infra/HttpClient/HttpClient'
+import { tokenSVC } from '../../src/services/auth/tokenSVC'
 
 const NEXT_REFRESH_TOKEN = 'REFRESH_TOKEN_NAME'
 
@@ -29,11 +31,45 @@ const controllers = {
       },
     })
   },
+
+  async regenTokens(req, res) {
+    const ctx = { req, res }
+    const cookies = nookies.get(ctx)
+    const refresh_token = cookies[NEXT_REFRESH_TOKEN]
+
+    const resHTTP = await HttpClient(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/refresh`,
+      {
+        method: 'POST',
+        body: {
+          refresh_token,
+        },
+      }
+    )
+
+    tokenSVC.save(resHTTP.body.data.access_token, ctx)
+
+    if (resHTTP.ok) {
+      nookies.set(ctx, NEXT_REFRESH_TOKEN, resHTTP.body.data.refresh_token, {
+        httpOnly: true,
+        sameSite: 'lax',
+      })
+
+      res.json({ resHTTP })
+    } else {
+      res.json({
+        error: {
+          status: 401,
+          message: 'Não autorizado',
+        },
+      })
+    }
+  },
 }
 
 const controllerBy = {
   POST: controllers.saveRefreshToken,
-  GET: controllers.displayCookies /** NUNCA deve ir para produção */,
+  GET: controllers.regenTokens /** NUNCA deve ir para produção */,
 }
 
 export default function handler(req, res) {
